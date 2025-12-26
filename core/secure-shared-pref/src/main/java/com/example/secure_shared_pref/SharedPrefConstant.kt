8ccb93b7346de02ed000297163667f6ebb099144
mojtaba <mojtaba.joshaghani99@gmail.com>
2025-12-26 11:22:40 +0330

    finish shared-pref

---
+++ core/secure-shared-pref/src/main/java/com/example/secure_shared_pref/SharedPrefConstant.kt
@@ -0,0 +1,5 @@
+package com.example.secure_shared_pref
+
+object SharedPrefConstant {
+    const val TOKEN="token"
+}
---
+++ core/secure-shared-pref/src/main/java/com/example/secure_shared_pref/di/EncryptSharedModule.kt
@@ -0,0 +1,33 @@
+package com.example.secure_shared_pref.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object EncryptSharedModule {
+
+    @Singleton
+    @Provides
+    fun provideEncryptSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        return EncryptedSharedPreferences.create(
+            /* context = */ context,
+            /* fileName = */ "secret_shared_prefs",
+            /* masterKey = */ masterKey,
+            /* prefKeyEncryptionScheme = */ EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            /* prefValueEncryptionScheme = */ EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+}
